a6c4fcd5616082e48af4b6672cd390c2fcd9d9fb
TanviDhiwar <tanvidhiwar6@gmail.com>
2025-08-21 17:31:48 +0530

    Delete test1 directory

--- test1/Q1.py
+++
@@ -1,9 +0,0 @@
-l = int(input('Enter the length:'))
-b = int(input('Enter the breadth:'))
-r = int(input('Enter the Radius:'))
-
-area = (l * b) + (1/2 * 3.14 * r * r)
-peri = (2 * b) + l + (3.14 * r)
-
-print(f'Area of figure:{area}')
-print(f'Perimeter of figure:{peri}')
--- test1/Q2SI.py
+++
@@ -1,7 +0,0 @@
-p = int(input('Enter Principal amt:'))
-r = int(input('Enter Rate:'))
-t = int(input('Enter Time:'))
-
-SI = p * r * t / 100
-
-print(f'Simple Interest is:{SI}')
