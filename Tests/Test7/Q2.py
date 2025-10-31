7eec0768552cfb1b707b1d4f6c817edc3f50e6f8
TanviDhiwar <tanvidhiwar6@gmail.com>
2025-10-31 12:30:03 +0530

    Add files via upload

---
+++ Tests/Test7/Q2.py
@@ -0,0 +1,5 @@
+strings = ['Tanvi', 'Rajendra', 'Dhiwar']
+print('String As a Input:',strings)
+
+upper_list = [s.upper() for s in strings]
+print('String In Uppercase Version :',upper_list)
---
+++ Tests/Test7/Q3.py
@@ -0,0 +1,5 @@
+odd_no = [num for num in range(1, 51) if num % 2 != 0]
+print("ODD NUMBERS:", odd_no)
+
+even_no = [num for num in range(1, 51) if num % 2 == 0]
+print("EVEN NUMBERS:", even_no)
